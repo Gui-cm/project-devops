88847323ea92e5a9d910173977cd761c1be4ed0e
Guilherme Moraes <guilherme.castilho@outlook.com>
2022-07-08 03:53:06 -0300

    Update s3

--- terraform/aws/us-east-1/infra/s3/provider.tf
+++ terraform/aws/us-east-1/infra/s3/provider.tf
@@ -2,7 +2,4 @@ provider "aws" {
   version = ">= 3.75.0"
   region  = "us-east-1"
   profile = "project-devops"
-  #assume_role {
-    #role_arn = "arn:aws:iam::590284024382:role/admin-role"
-  #}
 }
