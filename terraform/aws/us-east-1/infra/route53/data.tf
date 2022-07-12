04d71b62ab47460bb472a5b2aad1e5ac7444028f
Guilherme Moraes <guilherme.castilho@outlook.com>
2022-07-12 03:36:18 -0300

    Add records into r53

--- terraform/aws/us-east-1/infra/route53/data.tf
+++ terraform/aws/us-east-1/infra/route53/data.tf
@@ -2,3 +2,7 @@ data "aws_route53_zone" "guicmtech" {
   name         = "guicmtech.com.br"
   private_zone = false
 }
+
+data "aws_cloudfront_distribution" "cdn_arn" {
+  id = "E3H66VI2D8BVB3"
+}
--- terraform/aws/us-east-1/infra/route53/records.tf
+++ terraform/aws/us-east-1/infra/route53/records.tf
@@ -6,10 +6,26 @@ module "records" {
   zone_id = data.aws_route53_zone.guicmtech.zone_id
   records = [
     {
-      name    = "wwww"
+      name    = "www"
       type    = "CNAME"
       ttl     = 300
-      records = ["guicmtech.com.br"]
+      records = data.aws_cloudfront_distribution.cdn_arn.domain_name
+    }
+  ]
+}
+
+module "records_api" {
+  source = "github.com/terraform-aws-modules/terraform-aws-route53//modules/records?ref=v2.0.0"
+
+  create = true
+
+  zone_id = data.aws_route53_zone.guicmtech.zone_id
+  records = [
+    {
+      name    = "api"
+      type    = "CNAME"
+      ttl     = 300
+      records = "sbadjasdbnjak"
     }
   ]
 }
