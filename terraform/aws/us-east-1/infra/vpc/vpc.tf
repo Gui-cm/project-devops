a3085574dcd653ba3667dc5ba85f94251597f331
Guilherme Moraes <guilherme.castilho@outlook.com>
2022-07-12 03:14:01 -0300

    vpc and subnets pub/prv with igw/nat/rtb

--- terraform/aws/us-east-1/infra/vpc/vpc.tf
+++ terraform/aws/us-east-1/infra/vpc/vpc.tf
@@ -1,18 +1,32 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+data "aws_availability_zones" "available_zones" {
+  state = "available"
+}
 
-  create_vpc = true
+resource "aws_vpc" "ecs-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "${var.tags.Name}"
+  }
+}
 
-  name = lower(var.tags.Name)
-  cidr = "10.0.0.0/16"
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.ecs-vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.ecs-vpc.cidr_block, 8, 2 + count.index)
+  availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet-public"
+  }
+}
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
-
-  tags = var.tags
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.ecs-vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.ecs-vpc.cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
+  tags = {
+    Name = "subnet-private"
+  }
 
 }
