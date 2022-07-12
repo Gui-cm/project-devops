e461ece26f6a8df3f1cb7292c43536b5da105e9a
Guilherme Moraes <guilherme.castilho@outlook.com>
2022-07-12 03:37:42 -0300

    Add cluster project-devops

--- terraform/aws/us-east-1/infra/ecs/ecs.tf
+++ terraform/aws/us-east-1/infra/ecs/ecs.tf
@@ -3,21 +3,24 @@ module "ecs" {
 
   cluster_name = lower("cluster-${var.tags.Name}")
 
-  autoscaling_capacity_providers = {
-    asg = {
-      auto_scaling_group_arn         = module.asg.autoscaling_group_arn
-      managed_termination_protection = "DISABLED"
-
-      managed_scaling = {
-        maximum_scaling_step_size = 2
-        minimum_scaling_step_size = 1
-        status                    = "ENABLED"
-        target_capacity           = 60
+  cluster_configuration = {
+    execute_command_configuration = {
+      logging = "OVERRIDE"
+      log_configuration = {
+        cloud_watch_log_group_name = "/aws/ecs/aws-ec2"
       }
+    }
+  }
 
+  fargate_capacity_providers = {
+    FARGATE = {
       default_capacity_provider_strategy = {
-        weight = 60
-        base   = 20
+        weight = 50
+      }
+    }
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = 50
       }
     }
   }
